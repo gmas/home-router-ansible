e726d62adcbe0439ae7e8ca3199110b44bc7661a
George Masgras <gmasgras@gmail.com>
2015-01-25 20:11:29 -0800

    add AirVPN config and hook script for generating .info file in dhcpcd dir

---
+++ roles/router/files/openvpn/save_gateway.rb
@@ -0,0 +1,9 @@
+#!/bin/env ruby
+# saves IP as gateway in dhcpcd client format
+# used by Shorewall to 'detect' provider gateway
+#echo "GATEWAYS=$5" > /var/lib/dhcpcd/dhcpcd-tun1.info
+gateway = ARGF.argv[4]
+File.open("/var/lib/dhcpcd/dhcpcd-tun1.info", "r+") do |f|
+  f.truncate(0)
+  f.write "GATEWAYS=#{gateway}\r\n"
+end
