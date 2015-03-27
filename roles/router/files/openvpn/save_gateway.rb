3785e7af68f8a8fc612934f333e05475c07a59b5
George Masgras <gmasgras@gmail.com>
2015-03-26 18:31:41 -0700

    update OpenVPN save_gateway script to account for change in the way AirVPN sends gateway info

--- roles/router/files/openvpn/save_gateway.rb
+++ roles/router/files/openvpn/save_gateway.rb
@@ -2,7 +2,7 @@
 # saves IP as gateway in dhcpcd client format
 # used by Shorewall to 'detect' provider gateway
 #echo "GATEWAYS=$5" > /var/lib/dhcpcd/dhcpcd-tun1.info
-gateway = ARGF.argv[4]
+gateway = ARGF.argv[3]
 File.open("/var/lib/dhcpcd/dhcpcd-tun1.info", "r+") do |f|
   f.truncate(0)
   f.write "GATEWAYS=#{gateway}\r\n"
