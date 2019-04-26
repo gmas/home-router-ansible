b02b7a4653a0fe2dadf9d0c27eefa951c4159ad3
George Masgras <gmasgras@gmail.com>
2019-04-26 14:47:37 -0700

    [router]
    - switch vpn interface to tun0
    - add eth2 as LAN interface
    - allow DLNA (1900 UDP) from FW to LAN
    - DROP traffic from 192.168.20.128/27 through eth1 (TWC)
    - update shell failed check to new Ansible

--- roles/router/files/openvpn/save_gateway.rb
+++ roles/router/files/openvpn/save_gateway.rb
@@ -1,13 +1,12 @@
 #!/bin/env ruby
 # saves IP as gateway in dhcpcd client format
 # used by Shorewall to 'detect' provider gateway
-#echo "GATEWAYS=$5" > /var/lib/dhcpcd/dhcpcd-tun1.info
+#echo "GATEWAYS=$5" > /var/lib/dhcpcd/dhcpcd-tun0.info
 gateway = ARGF.argv[3]
-File.open("/var/lib/dhcpcd/dhcpcd-tun1.info", "r+") do |f|
+File.open("/var/lib/dhcpcd/dhcpcd-tun0.info", "r+") do |f|
   f.truncate(0)
   f.write "GATEWAYS=#{gateway}"
 end
 
 # reload shorewall
 system("/usr/bin/shorewall restart")
-
