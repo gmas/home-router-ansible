1b243b09e393d10c0288bf9681b82fabd7aa06fd
George Masgras <gmasgras@gmail.com>
2020-07-19 16:08:42 -0700

    upgrade Metal LB

--- kubernetes-arm/1-MetalLB/install_metallb.sh
+++ kubernetes-arm/1-MetalLB/install_metallb.sh
@@ -1,1 +1,7 @@
-kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.4.5/manifests/metallb.yaml
+#! /bin/bash
+
+kubectl apply -f ./metallb-namespace.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+kubectl apply -f ./metallb.yaml
+kubectl apply -f ./metallb-conf.yaml
